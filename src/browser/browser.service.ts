66840ee160ad30d99d557f1803020615ff0c22bd
Tian <jacky841224j@gmail.com>
2025-01-18 17:05:36 +0800

    modify:調整初始化瀏覽器時間

--- src/browser/browser.service.ts
+++ src/browser/browser.service.ts
@@ -3,27 +3,36 @@ import puppeteer, { Browser, Page } from 'puppeteer';
 
 @Injectable()
 export class BrowserService {
-  browser: Browser;
-  page: Page;
-
-  async initBrowser() {
-    this.createBrowser();
+  constructor() { 
+    // this.createBrowser();
     this.createPage();
   }
 
-  async closeBrowser() {
+  browser: Browser;
+  page: Page;
+
+  // async initBrowser() {
+  //   this.createBrowser();
+  //   this.createPage();
+  // }
+
+  async disposeBrowser() {
+    await this.page?.close();
     await this.browser?.close();
   }
 
   private async createBrowser() {
     try {
-      this.browser = await puppeteer.launch({
-        headless: true,
-        args: ['--no-sandbox', '--disable-setud-sandbox'],
-      });
+      if (this.browser == null) {
+        this.browser = await puppeteer.launch({
+          headless: true,
+          args: ['--no-sandbox', '--disable-setuid-sandbox']
+        });
 
-      if (this.browser == null) throw new Error('Browser建立失敗');
-      console.log('Browser建立成功');
+        if (this.browser == null) throw new Error('Browser建立失敗');
+        console.log('Browser建立成功');
+      }
+
     } catch (error: any) {
       await this.browser?.close();
       throw new Error(`[createBrowser] : ${error.message}`);
@@ -33,12 +42,14 @@ export class BrowserService {
   private async createPage() {
     try {
       if (this.browser == null) await this.createBrowser();
+      if (this.page == null) {
+        this.page = await this.browser.newPage();
+        await this.page.setViewport({ width: 1920, height: 1080 });
 
-      this.page = await this.browser.newPage();
-      await this.page.setViewport({ width: 1920, height: 1080 });
+        if (this.page == null) throw new Error('Page建立失敗');
+        console.log('Page建立成功');
+      }
 
-      if (this.page == null) throw new Error('Page建立失敗');
-      console.log('Page建立成功');
     } catch (error: any) {
       throw new Error(`[createPage] : ${error.message}`);
     }
