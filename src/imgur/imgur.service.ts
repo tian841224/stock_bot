512933e902894c9b7f92427016dcef446fa35207
Tian <jacky841224j@gmail.com>
2025-01-28 01:39:43 +0800

    modify:IMGUR_CLIENT_ID改為從env匯入

--- src/imgur/imgur.service.ts
+++ src/imgur/imgur.service.ts
@@ -1,29 +1,30 @@
 import { HttpService } from '@nestjs/axios';
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { firstValueFrom } from 'rxjs';
 import { ImgurResponseDto } from './interface/Imgur-response-dto';
 
 @Injectable()
 export class ImgurService {
-    private readonly clientId = 'bd4a58883b0eda8';
-    private readonly baseUrl = 'https://api.imgur.com/3';
+  @Inject('IMGUR_CLIENT_ID') 
+  private readonly clientId: string
+  private readonly baseUrl = 'https://api.imgur.com/3';
 
-    constructor(private readonly httpService: HttpService) {}
-    
-    async uploadImage(image: Buffer): Promise<ImgurResponseDto> {
-        const url = `${this.baseUrl}/image`;
-        const headers = {
-          Authorization: `Client-ID ${this.clientId}`,
-        };
-    
-        const formData = new FormData();
-        formData.append('image', image.toString('base64'));
-    
-        const response = await firstValueFrom(
-          this.httpService.post(url, formData, { headers }),
-        );
-    
-        const result: ImgurResponseDto = response.data.data;
-        return result;
-      }
+  constructor(private readonly httpService: HttpService) { }
+
+  async uploadImage(image: Buffer): Promise<ImgurResponseDto> {
+    const url = `${this.baseUrl}/image`;
+    const headers = {
+      Authorization: `Client-ID ${this.clientId}`,
+    };
+
+    const formData = new FormData();
+    formData.append('image', image.toString('base64'));
+
+    const response = await firstValueFrom(
+      this.httpService.post(url, formData, { headers }),
+    );
+
+    const result: ImgurResponseDto = response.data.data;
+    return result;
+  }
 }
