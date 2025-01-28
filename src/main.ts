0b19228570f5110c8545a1df376235e2cfbcf41c
Tian <jacky841224j@gmail.com>
2025-01-28 23:35:19 +0800

    feat:加入gzip壓縮功能、移除未使用套件

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as compression from 'compression';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
@@ -8,6 +9,8 @@ async function bootstrap() {
   });
   let port = process.env.PORT ?? 3000;
   // app.use(multer().any());
+  app.use(compression());  // 啟用壓縮
+
   await app.listen(port);
 
   console.log(`app listening at http://localhost:${port}`);
