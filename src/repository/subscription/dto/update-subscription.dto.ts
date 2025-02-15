5f80d6d34572c1f957377dd10064690484dc00d1
Tian <jacky841224j@gmail.com>
2025-02-15 23:36:21 +0800

    fix:修正SubscriptionDto status設定位置錯誤

--- src/repository/subscription/dto/update-subscription.dto.ts
+++ src/repository/subscription/dto/update-subscription.dto.ts
@@ -1,5 +1,11 @@
-import { OmitType, PartialType } from '@nestjs/swagger';
+import { ApiProperty, OmitType, PartialType } from '@nestjs/swagger';
 import { CreateSubscriptionDto } from './create-subscription.dto';
+import { IsNumber, IsOptional } from 'class-validator';
 
 export class UpdateSubscriptionDto extends PartialType(
-    OmitType(CreateSubscriptionDto, ['userId'] as const)) { }
+    OmitType(CreateSubscriptionDto, ['userId'] as const)) {
+    @ApiProperty({ description: '狀態 0:關閉, 1:開啟' })
+    @IsNumber()
+    @IsOptional()
+    status?: number;
+}
