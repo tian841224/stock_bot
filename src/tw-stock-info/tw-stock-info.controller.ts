01e08e7e50e69519b26cb261bde0f6a51dc6e572
Tian <jacky841224j@gmail.com>
2025-03-02 11:06:32 +0800

    modify:新增controller swagger 敘述、移除不需使用功能

--- src/tw-stock-info/tw-stock-info.controller.ts
+++ src/tw-stock-info/tw-stock-info.controller.ts
@@ -1,27 +1,35 @@
 import { Controller, Get, Query } from '@nestjs/common';
 import { TwStockInfoService } from './tw-stock-info.service';
+import { ApiOperation, ApiParam } from '@nestjs/swagger';
 
 @Controller('tw-stock-info')
 export class TwStockInfoController {
-  constructor(private readonly twStockInfoService: TwStockInfoService) {}
+  constructor(private readonly twStockInfoService: TwStockInfoService) { }
 
+  @ApiOperation({ summary: '取得今日大盤行情' })
+  @ApiParam({ name: 'count', required: false, description: '取得幾天內的資料' })
   @Get('getDailyMarketInfo')
   async getDailyMarketInfo(@Query('count') param?: number) {
     return await this.twStockInfoService.getDailyMarketInfoAsync(param);
   }
 
+  @ApiOperation({ summary: '取得盤後個股資訊' })
+  @ApiParam({ name: 'symbol', required: true, description: '輸入股票代號' })
   @Get('getAfterTradingVolume')
-  async getAfterTradingVolume(@Query('symbol') param?: string) {
+  async getAfterTradingVolume(@Query('symbol') param: string) {
     return await this.twStockInfoService.getAfterTradingVolumeAsync(param);
   }
 
+  @ApiOperation({ summary: '取得成交量前20股票' })
   @Get('getTopVolumeItems')
   async getTopVolumeItems() {
     return await this.twStockInfoService.getTopVolumeItemsAsync();
   }
 
+  @ApiOperation({ summary: '取得個股新聞' })
+  @ApiParam({ name: 'symbol', required: true, description: '輸入股票代號' })
   @Get('getStockNews')
-  async getStockNews(@Query('symbol') param?: string) {
+  async getStockNews(@Query('symbol') param: string) {
     return await this.twStockInfoService.getStockNewsAsync(param);
   }
 }
